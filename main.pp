5d0e1f34b92e74daf03db7df96bed6553eb7bf43
Pinnapong Silpsakulsuk <ping@abctech-thailand.com>
2014-04-21 14:01:48 +0700

    Add git home directory and puppet directory

--- main.pp
+++ main.pp
@@ -2,3 +2,10 @@ user { "git":
     ensure => "present",
     home => "/var/git",
 }
+
+file {
+    "/var/git": ensure => directory, owner => git,
+    require => User["git"];
+    "/var/git/puppet": ensure => directory, owner => git,
+    require => [User["git"], File["/var/git"]],
+}
