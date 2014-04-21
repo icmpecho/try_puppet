ccc88f8ea702ef5dc34c0ecb06416e71696d49d1
Pinnapong Silpsakulsuk <ping@abctech-thailand.com>
2014-04-21 14:35:53 +0700

    Fix user

--- main.pp
+++ main.pp
@@ -12,6 +12,7 @@ file {
 
 ssh_authorized_key { "git":
     ensure => present,
+    user => "git",
     key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCiqB/STbvnEWRpMYkm9/w3ukTbxxosQUZMQKVlO0PQ3G1IGdEhPUN0viaHNMFXm0o4LyZKWPenEgu1W5dbpfBMgi7rEXsHcw2EDglPWCC5u+JpKhJm+b9hr3gbL8oREiJCbW09BW3jFdXIZA0lAZehTlouAnppI/+9cIacCVXeod94t+/ao3D8C05t0IQePzDGwYOLfOrGoLXjUxMczyeshVf6JVHZHdbMhIPQkb24bmWMQc3BB7wNZiYdB5Xqr+mDPpKVr4bdGV+AVxd+pLbUagqMfQrXs+DruDUuC9IioqnQu+pXVFdkRJGaR/mXAOlQlPhTfNlh+Krb/WgnZPQZ",
     name => "Ping@abctech-thailand.com",
     target => "/var/git/.ssh/authorized_keys",
